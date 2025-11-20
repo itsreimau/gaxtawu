97fdd3420052637cdde55a7c9bcbbc4b5627c880
itsreimau <private.iblmln@gmail.com>
2025-11-21 05:49:50 +0700

    feat(commands/misc): update collector.js to handle invalid timeout values

--- commands/misc/collector.js
+++ commands/misc/collector.js
@@ -1,29 +1,32 @@
 const util = require("node:util");
 
 module.exports = {
-        name: "collector",
-        aliases: ["collect"],
-        category: "misc",
-        code: async (ctx) => {
-            const timeout = parseInt(ctx.args[0]) || 60000;
-            if (isNaN(timeout)) return await ctx.reply(`ⓘ ${formatter.italic("Waktu timeout harus berupa angka!")}`);
+    name: "collector",
+    aliases: ["collect"],
+    category: "misc",
+    code: async (ctx) => {
+        const timeout = parseInt(ctx.args[0]) || 60000;
+        if (isNaN(timeout)) return await ctx.reply(`ⓘ ${formatter.italic("Waktu timeout harus berupa angka!")}`);
 
-            try {
-                const collector = ctx.MessageCollector({
-                    time: timeout
-                });
-                await ctx.reply(`ⓘ ${formatter.italic(`Collector dimulai dengan timeout ${tools.msg.convertMsToDuration(timeout)}`)}`);
+        try {
+            const collector = ctx.MessageCollector({
+                time: timeout
+            });
+            await ctx.reply(`ⓘ ${formatter.italic(`Collector dimulai dengan timeout ${tools.msg.convertMsToDuration(timeout)}`)}`);
 
-                collector.on("collect", async (m) => {
-                    await ctx.core.sendMessage(ctx.id, {
-                        text: util.inspect(m)
-                    }, {
-                        quoted: m
-                    });
+            collector.on("collect", async (m) => {
+                await ctx.core.sendMessage(ctx.id, {
+                    text: util.inspect(m)
+                }, {
+                    quoted: m
                 });
+            });
 
-                collector.on("end", async () => {
-                    await ctx.reply(`ⓘ ${formatter.italic("Collector berhenti!")}`);
-                });
-            }
-        };
+            collector.on("end", async () => {
+                await ctx.reply(`ⓘ ${formatter.italic("Collector berhenti!")}`);
+            });
+        } catch (error) {
+            await tools.cmd.handleError(ctx, error);
+        }
+    }
+};
