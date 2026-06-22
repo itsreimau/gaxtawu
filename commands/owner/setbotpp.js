1872807987cfa037c3c7aececd604aa5a90195f3
Sir Kafka <107709019+SirKaff@users.noreply.github.com>
2026-06-22 08:30:18 +0700

    halo

--- commands/owner/setbotpp.js
+++ commands/owner/setbotpp.js
@@ -16,7 +16,7 @@ module.exports = {
         try {
             const buffer = await ctx.msg.download() || await ctx.quoted.download();
             const image = checkMedia ? ctx.msg.message.imageMessage : ctx.quoted.message.imageMessage;
-            const dimensions = tools.calculateDimensions(image.width, image.height);
+            const dimensions = tools.cmd.calculateDimensions(image.width, image.height);
             await ctx.core.updateProfilePicture(ctx.me.id, buffer, dimensions);
 
             await ctx.reply(tools.msg.info("Berhasil mengubah gambar profil bot!"));
