82808ff4ce0a2e703d1537388a21f846cf2b91c8
itsreimau <private.iblmln@gmail.com>
2026-07-17 16:26:47 +0700

    chore: remove unused Formatter module

--- engine/index.js
+++ engine/index.js
@@ -3,7 +3,6 @@ const CommandHandler = require("./Classes/CommandHandler.js");
 const Config = require("./Classes/Config.js");
 const Cooldown = require("./Classes/Cooldown.js");
 const Events = require("./Constant/Events.js");
-const Formatter = require("./Helper/Formatter.js");
 const MessageType = require("./Constant/MessageType.js");
 const VCardBuilder = require("./Classes/Builder/VCard.js");
 
@@ -13,7 +12,6 @@ module.exports = {
     Config,
     Cooldown,
     Events,
-    Formatter,
     MessageType,
     VCardBuilder
 };
